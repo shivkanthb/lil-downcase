8a677f848ba062fc2d48959e7fed6eeeac6b246c
shiv <shivkanthb@gmail.com>
2023-02-02 23:56:19 -0800

    default run on all websites

--- content.js
+++ content.js
@@ -1,31 +1,41 @@
 // document.getElementsByTagName("body")[0].style.backgroundColor = "red";
-// document.getElementsByTagName("body")[0].style.textTransform = "lowercase";
+document.getElementsByTagName("body")[0].style.textTransform = "lowercase";
 
-const styleEl = `<style>
-    .lowerit {
-      text-transform: lowercase;
-    }
-  </style>`;
-document.getElementsByTagName("head")[0].innerHTML += styleEl;
+// const styleEl = `<style>
+//     .lowerit {
+//       text-transform: lowercase;
+//     }
+//   </style>`;
+// // document.getElementsByTagName("head")[0].innerHTML += styleEl;
 
-function setDowncase() {
-  document.getElementsByTagName("body")[0].classList.add("lowerit");
-}
+// const globalStyleEl = `<style>
+//     * {
+//       text-transform: lowercase !important;
+//     }
+//   </style>`;
 
-// var lowercaseBtn = document.getElementById("lowercaseBtn");
-// lowercaseBtn.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({
-//     active: true,
-//     currentWindow: true,
-//   });
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setDowncase,
-//   });
+// function setDowncase() {
+//   // document.getElementsByTagName("head")[0].innerHTML += globalStyleEl;
+//   // document.getElementsByTagName("body")[0].classList.add("lowerit");
+
+//   document.getElementsByTagName("html")[0].style.cssText +=
+//     "text-transform: lowercase !important";
+// }
+
+// // var lowercaseBtn = document.getElementById("lowercaseBtn");
+// // lowercaseBtn.addEventListener("click", async () => {
+// //   let [tab] = await chrome.tabs.query({
+// //     active: true,
+// //     currentWindow: true,
+// //   });
+// //   chrome.scripting.executeScript({
+// //     target: { tabId: tab.id },
+// //     function: setDowncase,
+// //   });
+// // });
+
+// chrome.runtime.onMessage.addListener((message) => {
+//   if (message === "toggleCase") {
+//     setDowncase();
+//   }
 // });
-
-chrome.runtime.onMessage.addListener((message) => {
-  if (message === "toggleCase") {
-    setDowncase();
-  }
-});
